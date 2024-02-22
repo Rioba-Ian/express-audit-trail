063df09c437d62c26189e55d9b9bb7a059221c4c
Rioba-Ian <riobafelix@gmail.com>
2024-02-22 23:32:45 +0300

    first commit and add .gitgnore file

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/healthcheck", (req, res) => {
+ res.send("Ok.");
+});
+
+app.listen(port, () => {
+ console.log(`Server running on http://localhost:${port}`);
+});
